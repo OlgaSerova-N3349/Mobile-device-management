6ade0920f1edf2c31032d1c4b7ad6bfd033d8c6c
OlgaSerova-N3349 <OlgaSerovaN3349@users.noreply.github.com>
2020-04-12 08:30:01 +0300

    1

---
+++ Lab2/mob2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data=pd.read_excel('/Users/macbook/Documents/DATA.xlsx')
+
+print("Введите множитель тарифного плана k руб/Mb-",end='')
+k=int(input())
+
+print("Введите ip-адрес клиента-",end='')
+ip=input()
+
+X=k*data[data.IP_Addr==ip].BytesMb.values[0]-k*1
+print("Результат тарификации-",X,"руб.")
+
